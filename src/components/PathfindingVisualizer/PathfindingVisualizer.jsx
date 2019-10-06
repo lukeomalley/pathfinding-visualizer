3fc7f19c757c214f255bbccb1fcb8a0977bbbddb
Luke O'Malley <Lukeomalley95@gmail.com>
2019-10-05 22:16:51 -0400

    setup initial components and cleaned out CRA starter

---
+++ src/components/PathfindingVisualizer/PathfindingVisualizer.jsx
@@ -0,0 +1,15 @@
+import React, {useState} from 'react'
+
+import Node from '../Node'
+
+const PathfindingVisualizer = () => {
+  
+  return (
+    <div>
+     <h1>Pathfinding Visualizer</h1> 
+     <Node />
+    </div>
+  )
+}
+
+export default PathfindingVisualizer
