eb1c2dcfaf3f9f0998b7b92887ce6766c307fc2a
Luke O'Malley <Lukeomalley95@gmail.com>
2019-10-05 23:13:58 -0400

    setup initial grid rendering individual node elements

--- src/components/Node/Node.js
+++ src/components/Node/Node.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+
+const NodeWrapper = styled.div`
+  height: 20px;
+  width: 20px;
+  border: 1px solid blue;
+`;
 
 const Node = () => {
-  return (
-    <div>
-      <p>Node</p> 
-    </div>
-  )
-}
+  return <NodeWrapper></NodeWrapper>;
+};
 
-export default Node
+export default Node;
--- src/components/PathfindingVisualizer/PathfindingVisualizer.js
+++ src/components/PathfindingVisualizer/PathfindingVisualizer.js
@@ -1,7 +1,18 @@
 import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
 
 import Node from '../Node';
 import { createGrid } from '../../lib/createGrid';
+
+const NodeGrid = styled.div`
+  width: 90%;
+  margin: 0 auto;
+  display: grid;
+`;
+
+const RowWrapper = styled.div`
+  display: flex;
+`;
 
 const PathfindingVisualizer = () => {
   const [nodes, setNodes] = useState(createGrid());
@@ -15,9 +26,15 @@ const PathfindingVisualizer = () => {
   return (
     <div>
       <h1>Pathfinding Visualizer</h1>
-      {nodes.map(node => (
-        <Node />
-      ))}
+      <NodeGrid>
+        {nodes.map(row => (
+          <RowWrapper>
+            {row.map(row => (
+              <Node />
+            ))}
+          </RowWrapper>
+        ))}
+      </NodeGrid>
     </div>
   );
 };
